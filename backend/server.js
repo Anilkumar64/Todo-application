31fd46ab244838f7371d2ecdfff62773b4dbb075
Anil Reddy <vennapureddyanil456@gmial.com>
2025-11-06 13:21:25 +0530

    updated route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const { createTodo } = require("./types");
+const { parse } = require("zod");
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", (req, res) => {
-
+    const createparseload = req.body;
+    const parseload = createTodo.safeParse(createparseload);
+    if (!parseload.success) {
+        res.status(403).json({
+            msg: "you have some wrong inputs",
+        })
+        return;
+    }
 });
 
 app.get("/todos", (req, res) => {
@@ -12,7 +21,14 @@ app.get("/todos", (req, res) => {
 });
 
 app.put("/completed", (req, res) => {
-
+    const updateparseload = req.body;
+    const uparseload = createTodo.safeParse(createparseload);
+    if (!uparseload.success) {
+        res.status(403).json({
+            msg: "you have updated wrong inputs",
+        })
+        return;
+    }
 });
 
 app.listen(3000, () => {
